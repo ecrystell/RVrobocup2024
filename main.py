e1087de318bdc6068391dd92555b11c144cc6e7c
ecrystell <yeosugar6@gmail.com>
2024-01-10 21:06:04 +0800

    detects the black line before green

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ import numpy as np
 # GPIO.setup(40, GPIO.OUT)
 # GPIO.output(40, GPIO.HIGH)
 
+camera = cv2.VideoCapture(0) # webcam
+
+### IF USING RASPI UNCOMMENT LINES 16 TO 19
 # camera = PiCamera()
-camera = cv2.VideoCapture(0)
 # camera.resolution = (640, 360)
 # camera.rotation = 180
 #rawCapture = PiRGBArray(camera, size=(640, 360))
 time.sleep(0.1)
 
-# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):	
+# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True): ## UNCOMMENT THIS FOR RASPI
 while True:
 	Greendected = False # check whether got green
 
@@ -42,23 +44,53 @@ while True:
 	# finding contours of black and green
 	contours_blk, hierarchy_blk = cv2.findContours(Blackline.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	contours_grn, hierarchy_grn = cv2.findContours(Greensign.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	centerx_blk = 0
+
 	if len(contours_grn) > 0 :
-		Greendected = True	
+			
 		# drawing rect around the green square
 		x_grn, y_grn , w_grn, h_grn = cv2.boundingRect(contours_grn[0])
+		print(x_grn, y_grn, w_grn, h_grn)
 		centerx_grn = int(x_grn + (w_grn/2))
 
 		# drawing line in center of green square 
 		cv2.line(image, (centerx_grn, 200), (centerx_grn, 250),(0,0,255),3)	
+
+		# check if green is behind black line
+		checkimage = image[100:200-y_grn,  x_grn:(x_grn+w_grn)]
+		checkGreen = cv2.inRange(checkimage, (0,0,0), (50,50,50))
+		checkGreen = cv2.erode(checkGreen, kernel, iterations=5)
+		checkGreen = cv2.dilate(checkGreen, kernel, iterations=9)
+		contours_chk, hierarchy_chk = cv2.findContours(checkGreen.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		
+		if len(contours_chk)> 0:
+			Greendected = True
+			# im not gonna lie i dunno why the checkgreen works so if it doesnt send help
+			# xchk, ychk , wchk, hchk = cv2.boundingRect(contours_chk[0])
+			# centerx_chk = int(xchk + (wchk/2))  	   
+			# cv2.line(image, (centerx_chk, 100), (centerx_chk, 200-y_grn),(0,255,0),3)
+
 	if len(contours_blk) > 0 :
 		x_blk, y_blk , w_blk, h_blk = cv2.boundingRect(contours_blk[0])
 		centerx_blk = int(x_blk + (w_blk/2))  	   
 		cv2.line(image, (centerx_blk, 200), (centerx_blk, 250),(255,0,0),3)
+
 	if Greendected : 
-		if centerx_grn > centerx_blk :
+
+
+		# if centerx_grn > (centerx_blk + 20):
+		# 	cv2.putText(image, "Turn Right", (350,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),3)
+		# elif centerx_grn < (centerx_blk - 20) :
+		# 	cv2.putText(image, "Turn Left", (50,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),3)
+		# else:
+		# 	cv2.putText(image, "U-turn", (180,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),3)
+		
+		if centerx_grn > (centerx_blk + 20):
 			cv2.putText(image, "Turn Right", (350,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),3)
-		else :
+		elif centerx_grn < (centerx_blk - 20) :
 			cv2.putText(image, "Turn Left", (50,180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),3)
+
 	else :       
 		# pd controller kind of
 		setpoint = 320
@@ -67,9 +99,9 @@ while True:
 		cv2.putText(image, centertext, (200,340), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0),3)	
 
 	cv2.imshow("original with line", image)		
-	#rawCapture.truncate(0)	
+	#rawCapture.truncate(0)	### UNCOMMENT THIS FOR RASPI
 	key = cv2.waitKey(1) & 0xFF	
 	if key == ord("q"):
 		break
 
-# GPIO.output(40, GPIO.LOW)
+# GPIO.output(40, GPIO.LOW) ### UNCOMMENT THIS FOR RASPI (off the cam)
