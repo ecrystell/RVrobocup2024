d5ae271947b2280c45e7e6eacab7a7f5719e1d22
ecrystell <yeosugar6@gmail.com>
2024-01-31 19:57:26 +0800

    check rgb

---
+++ camerargb.py
@@ -0,0 +1,20 @@
+import time
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture(0) # webcam
+resolution = (320, 240)
+camera.set(3, resolution[0]) 
+camera.set(4, resolution[1]) 
+
+time.sleep(0.1)
+
+while True:
+	# getting the frame from vid capture
+	image = camera.read()[1]
+
+	print(image[120][160]) # DETECTING RGB VALUE IN THE MIDDLE
+	cv2.imshow("collecting rgb values", image)	
+	key = cv2.waitKey(1) & 0xFF	
+	if key == ord("q"):
+		break	
