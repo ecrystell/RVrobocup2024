3bde49a91a1317d00c0ae1da2bafe3a6c2d0d8e4
ecrystell <yeosugar6@gmail.com>
2024-03-08 16:14:28 +0800

    sadnotwork

--- lidartest.py
+++ lidartest.py
@@ -4,8 +4,13 @@ import cv2
 import time
 import serial
 import numpy as np
+import math
 
-
+def findeqns(x1, y1, x2, y2):
+	gradient = (y2-y1) / (x2-x1)
+	yintercept = y1 - (gradient * x1)
+	return (gradient, yintercept)
+	
 lidar = LD19('/dev/ttyAMA3', offsetdeg = 0, flip = True) #offsetddeg was -90
 robot = Robot('/dev/serial0')
 lidar.visualise(0, 180)
@@ -15,13 +20,15 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 200)
 # cv2.imshow("edges", edges)
 lines = cv2.HoughLines(edges, 3, np.pi/180, 110)
+weirdpts = []
+start = 0
 
 if lines is None:
-	pass
+	print("no img")
 else:
 	for line in lines:
 		# ~ x1,x2,y1,y2 = line[0]
-		# ~ cv2.line(img,(x1,y1), (x2,y2), (0,255,0), 2)
+		
 		rho,theta = line[0]
 		a = np.cos(theta)
 		b = np.sin(theta)
@@ -33,7 +40,18 @@ else:
 		y2 = int(y0 - 1000*(a))
 		
 		cv2.line(img,(x1,y1), (x2,y2), (0,255,0), 2)
-	
+		gradient, yintercept = findeqns(x1,y1,x2,y2)
+		for i in range(start, 180):
+			dist = lidar.lidarvalues[i]
+			pointx = (dist * 0.3 * math.cos(i/360 * 2 * math.pi + (math.pi))) + 360
+			pointy = (dist * 0.3 * math.sin(i/360 * 2 * math.pi + (math.pi))) + 360
+			
+			perpenddist = abs(( gradient * pointx - pointy + yintercept)/(gradient**2 +1) ** 0.5)
+			if perpenddist > 10:
+				if i - start < 20:
+					weirdpts.append(i)
+				start = i
+				break
 
 cv2.imshow("img", img)
 cv2.waitKey(0)
