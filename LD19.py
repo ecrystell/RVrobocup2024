2049b9c7056d5db362ed7247a8fefa065a4a3528
ecrystell <yeosugar6@gmail.com>
2024-03-13 18:05:45 +0800

    balllidar

--- LD19.py
+++ LD19.py
@@ -56,7 +56,7 @@ class LD19:
 	def __init__ (self, port, baud = 230400, offsetdeg = 0, flip = False):
 		self.readings = [0]* 360
 		self.lidarvalues = multiprocessing.Array('i',range(360))
-		self.lines = multiprocessing.Array('i', range(10))
+		# ~ self.lines = multiprocessing.Array('i', range(10))
 		self.port = port
 		self.flip = flip
 		self.offsetdeg = offsetdeg
@@ -114,7 +114,10 @@ class LD19:
 	def getReading(self, angle): # returns distance from the dot, one dot every degree
 		return self.lidarvalues[angle]
 	def visualise(self, startangle = 0, endangle = 180):
+		
 		if not self.visualisation:
+			# ~ queue = multiprocessing.Queue()
+			# ~ queue.put(self.lines)
 			self.startangle = (startangle) % 360
 			self.endangle = (endangle) % 360
 			# starts a separate thread by itself when run
@@ -128,7 +131,7 @@ class LD19:
 			self.visualisation = False
  
  
-	def find_best_fit_line(points):
+	def find_best_fit_line(self, points):
 		# Convert the list of points into NumPy arrays
 		points = np.array(points)
 		
@@ -156,24 +159,43 @@ class LD19:
 
 	def point_line_distance(self, point, A, B, C):
 		# Calculate the distance from the point to the line using the formula
-		distance = abs(A*point[0] + B*point[1] + C) / np.sqrt(A*2 + B*2)
+		distance = abs(A*point[0] + B*point[1] + C) / np.sqrt(A**2 + B**2)
 		return distance
 
 	def distance(self, point1, point2):
-		return ((point1[0]-point2[0])*2 + (point1[1]-point2[1])*2)*0.5
+		return ((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)**0.5
 
 	def RANSAC(self, laserdata, samples, maxDist, numberOfPoints=11): #samples is number of consec points to find best fit line
+		numpointsonlinelst = []
 		lines = []
 		laserdataLength = len(laserdata)
 		associatedReadings = [0] * len(laserdata)
 		startpoint = 0
+		# ~ print(len(laserdata))
+		startpts = []
+		colours = ["cyan", "magenta", "yellow", "green", "pink", "purple", "red"]
+		j=0
 		while startpoint< len(laserdata):
-			for i in range(startpoint, laserdataLength):
+			
+			count = 0
+			for i in range(startpoint, laserdataLength-1):
+				# ~ print(i)
 				if associatedReadings[i] == 0: 
-					startpoint = i
-					break
-				if i == laserdataLength:
+					dist = self.distance(laserdata[i], laserdata[i+1])
+					# ~ print(dist)
+					if dist > 10:
+						count = 0
+					else:
+						count += 1
+					if count == samples:
+						startpoint = i-samples +1
+						# ~ print('hello')
+						break
+				if i == laserdataLength-1:
 					startpoint = laserdataLength
+					# ~ print('hello')
+			startpts.append(startpoint)
+			
 			if startpoint+ samples > laserdataLength-1:
 				# print("no more samples")
 				break
@@ -181,27 +203,32 @@ class LD19:
 			numpointsonline = 0
 			linepoints = []
 			for i in range(startpoint, startpoint+samples):
+				#print(self.point_line_distance(laserdata[i], a, b, c))
 				if self.point_line_distance(laserdata[i], a, b, c) < maxDist: # check distance to line
 					numpointsonline += 1
 					linepoints.append(laserdata[i])
 					associatedReadings[i] = 1
+			#time.sleep(30)
+			# ~ print(numpointsonline)
 			if numpointsonline > numberOfPoints: # how many points u need to be on a line to be a line
 				cluster = numberOfPoints
 				clusterpoints = []
 				tempassosciatedReadings = []
 				for i in range(startpoint + samples, len(laserdata)):
+					
 					if self.point_line_distance(laserdata[i], a, b, c) < maxDist:
-						if self.distance(laserdata[i], linepoints[-1]) > 30: #if first point of new cluster, far away from old cluster
+						if self.distance(laserdata[i], linepoints[-1]) > 20: #if first point of new cluster, far away from old cluster
+						
 							cluster = 0
 							clusterpoints = []
 							clusterpoints.append(laserdata[i])
 							tempassosciatedReadings.append(i)
 						else:   # if there is a new cluster, we want multiple points in close proximity 
 							cluster += 1
-							if cluster < 3: # if there is less than 3 in the cluster, it may not be considered as in the same line as the current best fit, so take notes of it temporaryily first
+							if cluster < 7: # if there is less than 3 in the cluster, it may not be considered as in the same line as the current best fit, so take notes of it temporaryily first
 								tempassosciatedReadings.append(i)
 								clusterpoints.append(laserdata[i])
-							elif cluster == 3: # if cluster == 3, it is quite confirmed that it is on the line, add points and find new best fit
+							elif cluster == 7: # if cluster == 3, it is quite confirmed that it is on the line, add points and find new best fit
 								clusterpoints.append(laserdata[i])
 								tempassosciatedReadings.append(i)
 								linepoints.extend(clusterpoints)
@@ -212,20 +239,36 @@ class LD19:
 								linepoints.append(laserdata[i])
 								associatedReadings[i] = 1
 								a, b, c = self.find_best_fit_line(linepoints)
-				if self.distance(linepoints[0], linepoints[-1])> 50: # check distance of the line, if its long enough
+				
+				if self.distance(linepoints[0], linepoints[-1])> 30: # check distance of the line, if its long enough
+					a, b, c = self.find_best_fit_line(linepoints[:-1])
+					pygame.draw.circle(self.screen, colours[j], linepoints[0], 5)
+					pygame.draw.circle(self.screen, colours[j], linepoints[-2], 10)
+					
+					j+=1
+					j %= len(colours)
+					
 					lines.append((a, b, c))
-			startpoint += 2
-		self.lines = lines
+			startpoint += samples
+		# ~ for i in range(len(lines)):
+			# ~ self.lines[i] = lines[i]
+		
+		# ~ print(lines)
+		#print(numpointsonlinelst)
+		#print(startpts)
+		return lines
+		
 					
 		
-	def draw_lines(self, lines, color=(255, 255, 255), linewidth=2):
+	def draw_lines(self, lines, color=(0, 0, 0), linewidth=2):
+		
 		width  = 1200
 		center = (0,0)
 		for A, B, C in lines:
 			if B == 0:
                 # Handle the case when B is zero (line is horizontal)
 				x = -C / A
-				pygame.draw.line(self.map, color, (x, 0), (x, 720), linewidth)
+				pygame.draw.line(self.screen, color, (x, 0), (x, 720), linewidth)
 			else:
 				m = -A / B  # Slope
 				b = -C / B  # y-intercept
@@ -236,9 +279,10 @@ class LD19:
 				y2 = int(m * x2 + b)
 
 				# Draw the line on the screen
-				pygame.draw.line(self.map, color, (x1 + center[0], y1 + center[1]), (x2 + center[0], y2 + center[1]), linewidth)
+				pygame.draw.line(self.screen, color, (x1 + center[0], y1 + center[1]), (x2 + center[0], y2 + center[1]), linewidth)
 
 	def visualiseThread(self):
+		
 		pygame.init()
 		self.screen = pygame.display.set_mode((720, 720))
 		self.clock = pygame.time.Clock()
@@ -246,6 +290,8 @@ class LD19:
 		while running:
 			self.screen.fill("white")
 			pygame.draw.circle(self.screen, "green", (360, 360), 10)
+			pygame.draw.line(self.screen, "gray", (0,0),(360,360), 2)
+			pygame.draw.line(self.screen, "gray", (360,360),(720,0), 2)
 			#pygame.draw.arc(self.screen, "blue", ((0,0),(720,720)), 0, math.pi)
 			laserdata = []
 			for i in range(self.startangle, self.endangle):
@@ -254,9 +300,55 @@ class LD19:
 				pygame.draw.circle(self.screen, "red", (x, y), 2)
 				laserdata.append([x,y])
 
-			self.RANSAC(laserdata, 5, 10, 11)
-			self.draw_lines(self.lines)
-				
+			lines = self.RANSAC(laserdata, 15, 15, 14)
+			# ~ print(lines)
+			# ~ print(len(lines))
+			self.draw_lines(lines, "blue")
+			ballpts = []
+			
+			for i in range(45,135):
+				x = (self.lidarvalues[i] * 0.3 * math.cos(i/360 * 2 * math.pi + (math.pi))) + 360
+				y = (self.lidarvalues[i] * 0.3 * math.sin(i/360 * 2 * math.pi + (math.pi))) + 360
+				online = False
+				for A,B,C in lines:
+					dist = self.point_line_distance([x,y], A,B,C)
+					
+					if dist < 5:
+						online = True
+						break
+				if online == False:
+					ballpts.append(i)
+					
+			print(ballpts)
+			# use p turning to adjust ball to angle the ball is at
+			if len(ballpts) > 1:
+				ballstart = ballpts[0]
+				ballend = 0
+				ball = -1
+				for i in range(len(ballpts)-1):
+					if (ballpts[i] + 1 != ballpts[i+1]) or (i == len(ballpts)-2):
+						if i == len(ballpts)-2:
+							ballend = ballpts[-1]
+						else:
+							ballend = ballpts[i]
+						
+						x1 = (self.lidarvalues[ballstart] * 0.3 * math.cos(ballstart/360 * 2 * math.pi + (math.pi))) + 360
+						y1 = (self.lidarvalues[ballstart] * 0.3 * math.sin(ballstart/360 * 2 * math.pi + (math.pi))) + 360
+						
+						x2 = (self.lidarvalues[ballend] * 0.3 * math.cos(ballend/360 * 2 * math.pi + (math.pi))) + 360
+						y2 = (self.lidarvalues[ballend] * 0.3 * math.sin(ballend/360 * 2 * math.pi + (math.pi))) + 360
+						
+						dist = self.distance([x1,y1],[x2,y2])
+						# ~ print(dist)
+						if dist > 1 and dist < 13.5:
+							ball = (ballstart+ballend)//2
+							pygame.draw.circle(self.screen, "brown", (x1, y1), 10)
+							break
+						else:
+							ballstart = ballend
+						
+				print(ball)
+			
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					running = False
@@ -267,14 +359,17 @@ class LD19:
 		pygame.quit()
  
  
- 
+
 if __name__ == "__main__":
 	# offset is to change the starting degree
 	# if place the lidar right side up, 0 is left side clockwise to 360, so flip is 0 to 360 anticlockwise
 	lidar = LD19('/dev/ttyAMA3', offsetdeg = 0, flip = True) 
 	# use pygame to draw on the screen from 0 to 180
 	#this function is now non-blocking
-	lidar.visualise(10, 170) 
+	lidar.visualise(0, 180) 
+	
+	
+	
 	
     # use the lidar to check whether the left side or right side got more space to go
 	count = 0
